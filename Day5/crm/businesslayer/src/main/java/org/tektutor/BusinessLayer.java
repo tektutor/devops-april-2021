baff66f4b8ac9e2e32a0503b9ee2c291998c51e5
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-04-23 17:54:17 +0530

    Added Day5 lab exercises.

--- Day5/crm/businesslayer/src/main/java/org/tektutor/BusinessLayer.java
+++ Day5/crm/businesslayer/src/main/java/org/tektutor/BusinessLayer.java
@@ -2,26 +2,6 @@ package org.tektutor;
 
 public class  BusinessLayer  {
 
-public static List<String> zipSlipNoncompliant(ZipFile zipFile) throws IOException {
-    Enumeration<? extends ZipEntry> entries = zipFile.entries();
-    List<String> filesContent = new ArrayList<>();
-
-    while (entries.hasMoreElements()) {
-      ZipEntry entry = entries.nextElement();
-      File file = new File(entry.getName());
-      String content = FileUtils.readFileToString(file, StandardCharsets.UTF_8); // Noncompliant
-      filesContent.add(content);
-    }
-
-    return filesContent;
-  }
-
-protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-  String name = req.getParameter("name");
-  PrintWriter out = resp.getWriter();
-  out.write("Hello " + name); // Noncompliant
-}
-
 	public String getModuleName()  {
 		System.out.println ( "Business Layer ..." );
 		return "Business Layer";	
--- Day5/crm/frontend/src/main/java/org/tektutor/Frontend.java
+++ Day5/crm/frontend/src/main/java/org/tektutor/Frontend.java
@@ -1,15 +1,10 @@
 package org.tektutor;
-import java.utils.logging.*;
 
 public class  Frontend  {
 
 	public String getModuleName()  {
-		//System.err.println ( "FrontEnd  Layer ..." );
+		System.err.println ( "FrontEnd  Layer ..." );
 
-		Logger logger = Logger.getLogger ( GFG.class.getName() );
-		logger.log ( Level.INFO, "Entered getModuleName method ..." );
-		logger.log ( Level.WARNING, "This is a warning mmessage." );
-		
 
 		return "Frontend Layer";	
 	}
